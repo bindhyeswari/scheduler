10d8a48dd18eeb3f61d877cb54d387ae90bc8c5c
mishra <tech@peoplevendor.com>
2014-10-22 15:18:38 -0700

    test

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+var candidates = [];
+
 /* GET home page. */
-router.get('/', function(req, res) {
-    res.json(200, { message: 'Hello World!' });
+router.get('/json', function(req, res) {
+    res.status(200).json({ message: 'Hello World!' });
     // res.render('index', { title: 'Express' });
+});
+
+router.post('/json', function(req, res) {
+    console.log(req.body);
+    candidates.push(req.body);
+    res.status(200).json({ message: 'Thanks for the info!' });
+});
+
+router.get('/scheduler', function(req, res) {
+    res.render('scheduler');
 });
 
 module.exports = router;
